7ae4dca5e4bc86621e1789bbb625757ed71d4388
unknown <theopinheiro12@gmail.com>
2023-12-09 19:11:28 -0400

    apresentação inicial concluida e adcionado botão de saiba mais com efeito de glitch!

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -12,16 +12,31 @@ function Body(){
 
     return(
         <div className='apresentation'>
+
         <h1 className='my-name-is' 
         data-aos="fade-right"
         data-aos-duration="1000"
         data-aos-delay="500"
         data-aos-offset="200"
-        data-aos-easing="ease-in-out"
-        ><strong>Olá meu nome é <span className='name'>Theo Trindade</span> 
-            <br/>e sou um desenvolvedor fullstack!</strong> </h1>
+        data-aos-easing="ease-in-out">
+        <strong> 
+            Olá meu nome é <span className='name'>Theo Trindade</span> 
+            <br/>e sou um desenvolvedor fullstack!</strong></h1>
             
-            <button className="button-glitch" role="button">Saiba mais</button>
+            <button 
+            className="cybr-btn"
+            data-aos="fade-right"
+            data-aos-duration="1000"
+            data-aos-delay="2000"
+            data-aos-offset="200"
+            data-aos-easing="ease-in-out"
+
+            >Saiba <span aria-hidden>Mais</span>
+                <span aria-hidden className="cybr-btn__glitch">Saiba mais</span>
+                <span aria-hidden className="cybr-btn__tag">TT</span>
+            </button>
+
+
             {/* <button>Saiba mais</button> */}
             {/* <AwesomeButton type="primary">Saiba Mais</AwesomeButton>; */}
             
